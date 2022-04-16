8ae0e6be3b9942eb02fc1a1d99e90175eb9b9097
Tom <ckktom1999@gmail.com>
2022-04-16 15:08:21 +0800

    security login finish!

---
+++ Group_project/src/main/java/hkmu/comps380f/dao/CourseUserRepository.java
@@ -0,0 +1,9 @@
+package hkmu.comps380f.dao;
+
+import hkmu.comps380f.model.CourseUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CourseUserRepository extends JpaRepository<CourseUser, String> {
+
+}
